2dcf3c72c6246165936964e23b78412f39eb951d
Quangnt <thangteoxx@gmail.com>
2024-04-12 15:36:11 +0700

    acc

--- app.js
+++ app.js
@@ -17,6 +17,39 @@ const yourDate = new Date("2023-05-20T22:00:00"),
 document.addEventListener(
     "DOMContentLoaded",
     function () {
+        function waveLoadingEffect() {
+            var baseText = "C";
+            var waveText = "hào đón 365 ngày";
+            var element = document.querySelector(".loading-text");
+            var text = baseText + waveText;
+            var i = 0;
+            var forward = true;
+
+            function updateText() {
+                if (forward) {
+                    if (i < text.length) {
+                        element.textContent = text.substring(0, i + 1);
+                        i++;
+                    } else {
+                        forward = false;
+                        i--;
+                    }
+                } else {
+                    if (i >= baseText.length) {
+                        element.textContent = text.substring(0, i);
+                        i--;
+                    } else {
+                        forward = true;
+                        i++;
+                    }
+                }
+            }
+
+            setInterval(updateText, 350);
+        }
+
+        waveLoadingEffect();
+
         var rootTime = document.querySelector("time");
 
         document.querySelector("anni").textContent = `${
